fcdbc954e2203b47bb9793ed66f08955e26561ee
Genilson Monteiro da Silva <programationwork@gmail.com>
2025-07-13 22:59:36 -0300

    Add a funcao getElementsByClassName

---
+++ javascript/scriptClassName.js
@@ -0,0 +1,5 @@
+// Elementos do DOM
+const elemento = document.getElementsByClassName("piloto-f1-atual");
+for (let i = 0; i < elemento.length; i ++){
+    console.log(elemento[i].innerHTML);    
+}
